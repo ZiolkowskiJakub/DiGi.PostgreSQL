6870d85b3d1a8804f66e9a897651d5a377e3af15
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-06-30 22:56:55 +0200

    README.md updated and commandTimeout input added for ClearAsync

--- DiGi.PostgreSQL/Modify/ClearAsync.cs
+++ DiGi.PostgreSQL/Modify/ClearAsync.cs
@@ -12,13 +12,15 @@ namespace DiGi.PostgreSQL
         /// <param name="npgsqlConnection">The Npgsql connection to be used for the operation.</param>
         /// <param name="tableName">The name of the database table to clear.</param>
         /// <param name="cancellationToken">The cancellation token to observe while waiting for the task to complete.</param>
+        /// <param name="commandTimeout">The timeout in seconds for the execution of the command. A value of 0 disables the timeout.</param>
         /// <returns>A task that represents the asynchronous operation. The task result is true if the operation succeeded; otherwise, false.</returns>
-        public static async Task<bool> ClearAsync(NpgsqlConnection? npgsqlConnection, string tableName, CancellationToken cancellationToken = default)
+        public static async Task<bool> ClearAsync(NpgsqlConnection? npgsqlConnection, string tableName, CancellationToken cancellationToken = default, int commandTimeout = 30)
         {
             // Use TRUNCATE for speed, or DELETE for transactional safety
             string commandText = $"TRUNCATE TABLE {tableName} RESTART IDENTITY CASCADE;";
 
             using NpgsqlCommand command = new(commandText, npgsqlConnection);
+            command.CommandTimeout = commandTimeout;
             try
             {
                 await command.ExecuteNonQueryAsync(cancellationToken);
